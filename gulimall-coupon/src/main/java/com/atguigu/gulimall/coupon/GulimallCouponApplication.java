06b533152aa023634cb5726e4f9b6affb804ff21
TTWang <tiw210@ucsd.edu>
2020-10-05 22:01:17 -0700

    the creation of project

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
@@ -2,7 +2,12 @@ package com.atguigu.gulimall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/*
+* 每个微服务创建自己的命名空间，使用配置分组区分环境，dev, test, prod
+* */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallCouponApplication {
 
--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,36 @@
 package com.atguigu.gulimall.product;
 
-import org.junit.jupiter.api.Test;
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+@Slf4j
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallProductApplicationTests {
+public class GulimallProductApplicationTests  {
+
+	@Autowired
+	BrandService brandService;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+//		BrandEntity brandEntity = new BrandEntity();
+//		brandEntity.setBrandId(1L);
+//		brandEntity.setDescript("huawei shouji update");
+////		brandEntity.setName("华为");
+////		brandService.save(brandEntity);
+////		System.out.println("save successfully");
+//		brandService.updateById(brandEntity);
+		List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+		list.forEach(System.out::println);
 	}
 
 }

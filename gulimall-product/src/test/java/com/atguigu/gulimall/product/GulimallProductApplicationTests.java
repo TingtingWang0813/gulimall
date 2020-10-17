0baf8b3b1b5e95a6bb29ed093a2566a4f96aac0f
TTWang <tiw210@ucsd.edu>
2020-10-16 18:20:59 -0700

    the creation of project

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,8 +1,5 @@
 package com.atguigu.gulimall.product;
 
-import com.aliyun.oss.OSS;
-import com.aliyun.oss.OSSClient;
-import com.aliyun.oss.OSSClientBuilder;
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -13,9 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.List;
 
 @Slf4j
@@ -25,29 +19,6 @@ public class GulimallProductApplicationTests  {
 
 	@Autowired
 	BrandService brandService;
-
-	@Autowired
-	OSSClient ossClient;
-
-	@Test
-	public void testUpload() throws FileNotFoundException {
-//		// Endpoint以杭州为例，其它Region请按实际情况填写。
-//		String endpoint = "oss-us-west-1.aliyuncs.com";
-//// 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-//		String accessKeyId = "LTAI4G3M41o8pBFa6HpUz1zx";
-//		String accessKeySecret = "3ck58tlqsagj9SVTtUe5omweAswutx";
-//
-//// 创建OSSClient实例。
-//		OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-//
-// 上传文件流。
-		InputStream inputStream = new FileInputStream("/Users/wangtingting/Downloads/test.jpg");
-		ossClient.putObject("gulimall-wangme", "test.jpg", inputStream);
-
-// 关闭OSSClient。
-		ossClient.shutdown();
-		System.out.println("上传成功！！！");
-	}
 
 
 	@Test
---
+++ gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -0,0 +1,23 @@
+package com.atguigu.gulimall.thirdparty;
+
+import com.aliyun.oss.OSSClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GulimallThirdPartyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+
+}
